b3457828d05e05e571161b26b419dcd4d15cdb55
Yvettemukeshimana2 <yvettemukeshimana79@gmail.com>
2026-07-22 18:23:18 +0200

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,18 @@
  import { defineConfig } from "vite";
- import react from "@vitejs/plugin-react";
- 
+import react from "@vitejs/plugin-react";
 
- export default defineConfig({
-   plugins: [react()],
-   css: {
-     postcss: "./postcss.config.js",
-   },
-   build: {
-     
-       chunkSizeWarningLimit: 1000, // Set limit to 1000 kB
-      
-     rollupOptions: {
-       output: {
-         assetFileNames: "assets/[name]-[hash][extname]",
-       },
-     },
-   },
- });
+export default defineConfig({
+  plugins: [react()],
+  base: "./", // <--- Add this line right here
+  css: {
+    postcss: "./postcss.config.js",
+  },
+  build: {
+    chunkSizeWarningLimit: 1000, // Set limit to 1000 kB
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name]-[hash][extname]",
+      },
+    },
+  },
+});
